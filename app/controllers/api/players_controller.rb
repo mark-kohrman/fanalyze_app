6e849e8a57aaaf3c832d5b41e097ad5f022e9814
Mark-Kohrman <mark.kohrman@gmail.com>
2020-08-06 23:31:37 -0500

    adding create action

--- app/controllers/api/players_controller.rb
+++ app/controllers/api/players_controller.rb
@@ -8,4 +8,18 @@ class Api::PlayersController < ApplicationController
     @player = Player.find_by(id: params[:id])
     render 'show.json.jb'
   end
+
+  def create
+    @player = Player.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      position: params[:position],
+      nfl_team: params[:nfl_team],
+      projected_points: params[:projected_points],
+      weather_projected_points: params[:weather_projected_points], 
+      total_projected_points: params[:total_projected_points]
+    )
+    @player.save
+    render 'show.json.jb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     get "/players" => "players#index"
-    get "players/:id" => "players#show"
+    get "/players/:id" => "players#show"
+    post "/players" => "players#create"
   end
 end
