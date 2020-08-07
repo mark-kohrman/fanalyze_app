bbcde1d4cdc5ad8b8d8c6cb8f14e83093ba8b83c
Mark-Kohrman <mark.kohrman@gmail.com>
2020-08-07 10:44:06 -0500

    adding the update action

--- app/controllers/api/players_controller.rb
+++ app/controllers/api/players_controller.rb
@@ -10,6 +10,8 @@ class Api::PlayersController < ApplicationController
   end
 
   def create
+   
+
     @player = Player.new(
       first_name: params[:first_name],
       last_name: params[:last_name],
@@ -17,7 +19,22 @@ class Api::PlayersController < ApplicationController
       nfl_team: params[:nfl_team],
       projected_points: params[:projected_points],
       weather_projected_points: params[:weather_projected_points], 
-      total_projected_points: params[:total_projected_points]
+      total_projected_points: calculated_total
+    )
+    @player.save
+    render 'show.json.jb'
+  end
+
+  def update
+    @player = Player.find_by(id: params[:id])
+    @player.update(
+      first_name: params[:first_name] || @player.first_name,
+      last_name: params[:last_name] || @player.last_name,
+      position: params[:position] || @player.position,
+      nfl_team: params[:nfl_team] || @player.nfl_team,
+      projected_points: params[:projected_points] || @player.projected_points,
+      weather_projected_points: params[:weather_projected_points] || @player.weather_projected_points, 
+      total_projected_points: params[:total_projected_points] || @player.total_projected_points
     )
     @player.save
     render 'show.json.jb'
