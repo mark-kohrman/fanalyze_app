60181afa591f7914702c48a4535e4e34bcd4832e
Mark-Kohrman <mark.kohrman@gmail.com>
2020-08-06 22:58:41 -0500

    adding index and show actions for players

--- app/controllers/api/players_controller.rb
+++ app/controllers/api/players_controller.rb
@@ -1,5 +1,11 @@
 class Api::PlayersController < ApplicationController
   def index
+    @players = Player.all
     render 'index.json.jb'
+  end
+
+  def show
+    @player = Player.find_by(id: params[:id])
+    render 'show.json.jb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
   namespace :api do
     get "/players" => "players#index"
+    get "players/:id" => "players#show"
   end
 end
