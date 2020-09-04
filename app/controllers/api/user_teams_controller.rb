47d88c0e99b91dbaee33097fa299a42223f065f5
Mark-Kohrman <mark.kohrman@gmail.com>
2020-09-04 01:09:41 -0500

    working on update action in UserTeams

--- app/controllers/api/user_teams_controller.rb
+++ app/controllers/api/user_teams_controller.rb
@@ -57,19 +57,26 @@ class Api::UserTeamsController < ApplicationController
   def update
     p "$" * 88
     p current_user
-    p "$" * 88
+    qb_id = Player.find_by(player_name: params[:qb_player_id]).id
+    rb1_id = Player.find_by(player_name: params[:rb1_player_id]).id
+    rb2_id = Player.find_by(player_name: params[:rb2_player_id]).id
+    wr1_id = Player.find_by(player_name: params[:wr1_player_id]).id
+    wr2_id = Player.find_by(player_name: params[:wr2_player_id]).id
+    flex_id = Player.find_by(player_name: params[:flex_player_id]).id
+    te_id = Player.find_by(player_name: params[:te_player_id]).id
+    kicker_id = Player.find_by(player_name: params[:kicker_player_id]).id
 
     @user_team = UserTeam.find_by(id: params[:id])
     @user_team.update(
       user_team_name: params[:user_team_name] || @user_team.user_team_name,   
-      qb_player_id: params[:qb_player_id] || @user_team.qb_player_id,
-      rb1_player_id: params[:rb1_player_id] || @user_team.rb1_player_id,
-      rb2_player_id: params[:rb2_player_id] || @user_team.rb2_player_id,
-      wr1_player_id: params[:wr1_player_id] || @user_team.wr1_player_id,
-      wr2_player_id: params[:wr2_player_id] || @user_team.wr2_player_id,
-      flex_player_id: params[:flex_player_id] || @user_team.flex_player_id,
-      te_player_id: params[:te_player_id] || @user_team.te_player_id,
-      kicker_player_id: params[:kicker_player_id] || @user_team.kicker_player_id,
+      qb_player_id: qb_id || @user_team.qb_player_id,
+      rb1_player_id: rb1_id || @user_team.rb1_player_id,
+      rb2_player_id: rb2_id || @user_team.rb2_player_id,
+      wr1_player_id: wr1_id|| @user_team.wr1_player_id,
+      wr2_player_id: wr2_id || @user_team.wr2_player_id,
+      flex_player_id: flex_id || @user_team.flex_player_id,
+      te_player_id: te_id|| @user_team.te_player_id,
+      kicker_player_id: kicker_id || @user_team.kicker_player_id,
       user_id: current_user.id
 
     )
